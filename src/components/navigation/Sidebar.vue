18c77a186a5676101165a2374cb076b5a07831c1
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-06-28 19:40:04 -0700

    add: sidebar component

---
+++ src/components/navigation/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<ul>
+			<li>Elenor</li>
+			<li>
+				<i class="fas fa-plus-circle"></i>
+				<span>Add Art</span>
+			</li>
+			<li>
+				<span>Elenor</span>
+			</li>
+			<li>
+				<span>Profile</span>
+			</li>
+			<li>
+				<span>Inbox</span>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+    name: 'Sidebar'
+  };
+</script>
+
+<style>
+</style>
